425bbe28db073d77de70cd23038c0b936d75c4b7
lind1125 <danieljlinden@gmail.com>
2020-11-09 07:59:36 -0600

    stubbed out guest controller routes

---
+++ controllers/guest.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+// guest index route
+router.get('/', (req, res)=>{
+
+})
+
+// POST /guest create a new guest
+router.post('/', (req, res)=>{
+
+})
+
+// GET /guest/new - display form to create new guest
+router.get('/new', (req, res)=>{
+
+})
+
+module.exports = router
