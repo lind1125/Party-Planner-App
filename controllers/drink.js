9fb23b108a3ad19ca4ae724d64ed9ec31883bda2
lind1125 <danieljlinden@gmail.com>
2020-11-11 00:33:46 -0600

    moved drink search results to its own ejs to resolve reference error --need to investigate why that error did not seem to be happening with food

--- controllers/drink.js
+++ controllers/drink.js
@@ -3,10 +3,14 @@ const router = express.Router()
 const axios = require('axios')
 
 router.get('/', (req, res)=>{
-    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.keyword}`)
+    res.render('drink/main')
+})
+
+router.get('/results', (req, res)=>{
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.keyword}`) // query should be ${req.query.keyword}
     .then(response =>{
         let results = response.data.drinks
-        res.render('drink/main', {results: results})
+        res.render('drink/results', {results: results})
     })
     .catch(err=>{
         console.log('ERROR HAPPENING:', err)
