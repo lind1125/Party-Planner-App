e8e58a3e8e6e8fdca72fa2618e51deb1cc6c201a
lind1125 <danieljlinden@gmail.com>
2020-11-14 16:22:40 -0600

    built post route to add faves to faveRecipe model

--- controllers/food.js
+++ controllers/food.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const axios = require('axios')
 const isLoggedIn = require('../middleware/isLoggedIn')
 
+//GET route to display faved recipes and search form
 router.get('/', isLoggedIn, (req, res)=>{
     console.log(req.user.id)
     db.faveRecipe.findAll({
@@ -17,7 +18,7 @@ router.get('/', isLoggedIn, (req, res)=>{
         res.render('food/main', {faveRecipes: foundRecipes})
     })
 })
-
+//GET route to display search query results
 router.get('/results', (req, res)=>{
     axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${req.query.keyword}&apiKey=${process.env.FOOD_KEY}`)
     .then(response =>{
@@ -29,6 +30,28 @@ router.get('/results', (req, res)=>{
     })
 })
 
+// POST route to save recipes
+router.post('/:recipe_id/favorites', (req, res)=>{
+    let parameter = req.params.id
+    db.faveRecipe.findOrCreate({
+        where: {name: req.body.name,
+        image: req.body.image,
+        apiId: parseInt(parameter)}
+    })
+    .then(([faveRecipe, created])=>{
+        db.user.findByPk(1)
+        .then(foundUser=>{
+            foundUser.addFaveRecipe(faveRecipe)
+            console.log(`${foundUser.name} has added ${faveRecipe.name} to their favorites`)
+        })
+    })
+    .catch(err=>{
+        console.log("ERROR:", err)
+    })
+})
+
+
+//GET route to display information for an individual recipe
 router.get('/:recipe_id', (req, res)=>{
     axios.get(`https://api.spoonacular.com/recipes/${req.params.recipe_id}/information?includeNutrition=false&apiKey=${process.env.FOOD_KEY}`)
     .then(response =>{
@@ -37,9 +60,5 @@ router.get('/:recipe_id', (req, res)=>{
         res.render('food/show', {recipeInfo: recipeInfo})
     })
 })
-
-// POST route to save recipes
-
-//GET route to display saved recipes
 
 module.exports = router
