d9b6b38c17e79182c22976c90d3a98f8decd6b00
lind1125 <danieljlinden@gmail.com>
2020-11-14 15:06:48 -0600

    tested faverecipe model associations

--- dbTest.js
+++ dbTest.js
@@ -1,11 +1,20 @@
-// db.faveRecipe.findOrCreate({
-//     where: {name: 'Simba'},
-//     }
-// })
-// .then(([pet, created])=>{
-//     db.user.findByPk(5)
-//     .then(foundFatima=>{
-//         foundFatima.addPet(pet)
-//         console.log(`${foundFatima.firstName} is now the proud owner of ${pet.name}`)
-//     })
-// })
+const db = require('./models')
+const user = require('./models/user')
+const faveRecipe = require('./models/faverecipe')
+
+
+db.faveRecipe.findOrCreate({
+    where: {name: 'Pepperoni Pizza Muffins',
+    image: 'https://spoonacular.com/recipeImages/655698-556x370.jpg',
+    apiId: 655698}
+})
+.then(([faveRecipe, created])=>{
+    db.user.findByPk(1)
+    .then(foundUser=>{
+        foundUser.addFaveRecipe(faveRecipe)
+        console.log(`${foundUser.name} has added ${faveRecipe.name} to their favorites`)
+    })
+})
+.catch(err=>{
+    console.log("ERROR:", err)
+})
