3cdc0445ab668ea1d6948c2b1572f57f0ff73173
lind1125 <danieljlinden@gmail.com>
2020-11-14 18:17:46 -0600

    updated variable names to be more clear; added form to drink/show view

--- controllers/drink.js
+++ controllers/drink.js
@@ -9,8 +9,8 @@ router.get('/', (req, res)=>{
 router.get('/results', (req, res)=>{
     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.keyword}`) // query should be ${req.query.keyword}
     .then(response =>{
-        let results = response.data.drinks
-        res.render('drink/results', {results: results})
+        let drinkInfo = response.data.drinks
+        res.render('drink/results', {drinkInfo: drinkInfo})
     })
     .catch(err=>{
         console.log('ERROR HAPPENING:', err)
@@ -22,9 +22,9 @@ router.get('/:drink_id', (req, res)=>{
     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${req.params.drink_id}`)
     .then(response=>{
         // res.send(response.data)
-        let results = response.data.drinks[0]
-        // console.log(results.strDrink)
-        res.render('drink/show', {results: results})
+        let drinkInfo = response.data.drinks[0]
+        // console.log(drinkInfo.strDrink)
+        res.render('drink/show', {drinkInfo: drinkInfo})
     })
     .catch(err=>{
         console.log('ERROR HAPPENING:', err)
